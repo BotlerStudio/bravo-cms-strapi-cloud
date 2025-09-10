9b644f4183a02dc0e1f451de3696f5d9c3d70a88
AhmedOsamaWS <a.osama@waslabrowser.com>
2025-09-10 13:48:40 +0300

    BRAVO-12979 remove CTA background color attribute from announcement dashboard

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -396,7 +396,6 @@ export interface ApiAnnouncementCardAnnouncementCard
     createdBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
       Schema.Attribute.Private;
     ctaAr: Schema.Attribute.String & Schema.Attribute.Required;
-    ctaBackgroundColor: Schema.Attribute.String & Schema.Attribute.Required;
     ctaBackgroundTextColor: Schema.Attribute.String & Schema.Attribute.Required;
     ctaEn: Schema.Attribute.String & Schema.Attribute.Required;
     feedType: Schema.Attribute.Enumeration<['home', 'deals', 'wallet']> &
